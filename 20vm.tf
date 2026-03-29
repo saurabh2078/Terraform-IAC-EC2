f2dfa2e6654dd50c37b3143e59e56b2ed4a967f6
Ubuntu <ubuntu@ip-172-31-27-69.ap-southeast-2.compute.internal>
2026-03-29 10:00:29 +0000

    Terraform IAC EC2

--- 20vm.tf
+++ 20vm.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # ⚠️ Restrict in production
+    cidr_blocks = ["0.0.0.0/0"]   # Restrict in production
   }
 
   ingress {
